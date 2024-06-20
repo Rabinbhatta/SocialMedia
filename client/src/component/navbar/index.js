be3158d3cf7566c84c1c7d02be3bc4f46c737fb9
Rabinbhatta <rabinbhattarai646@gmail.com>
2024-06-20 10:43:26 +0545

    Navbar

--- client/src/component/navbar/index.js
+++ client/src/component/navbar/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 import "./styles.css"
 import { BiLogoFacebookCircle  } from "react-icons/bi";
 import { CiSearch } from "react-icons/ci";
+import { MdHomeFilled } from "react-icons/md";
+import { SiYoutubegaming } from "react-icons/si";
+import { MdGroups } from "react-icons/md";
+import { CiShop } from "react-icons/ci";
+import { MdOutlineOndemandVideo } from "react-icons/md";
+import { IoNotifications } from "react-icons/io5";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { CgMenuGridR } from "react-icons/cg";
 
 export const Navbar = () => {
   return (
@@ -11,6 +19,22 @@ export const Navbar = () => {
       <div className='search'>
             <CiSearch className='icons'/>
             <input placeholder='search facebook'/>
+      </div>
+      <div className='navMid'>
+      <MdHomeFilled />
+      <MdOutlineOndemandVideo/>
+      <CiShop/>
+      <SiYoutubegaming/>
+      <MdGroups/>
+      </div>
+
+      <div className='navEnd'>
+        <CgMenuGridR/>
+        <FaFacebookMessenger/>
+        <IoNotifications/>
+        <div className='profile'>
+          
+        </div>
       </div>
       </div>
     </div>
